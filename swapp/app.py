4922dfe07e6a3e5886953f4cd05f143a2d6bf90a
Denis Ziulikov <amznden@gmail.com>
2022-04-07 18:34:07 +0500

    Debug mode off

--- swapp/app.py
+++ swapp/app.py
@@ -110,5 +110,5 @@ def main_output(all_planets, all_characters):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=False)
 
