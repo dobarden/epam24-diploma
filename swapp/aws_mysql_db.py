182b7ab5cf3842c9ed5e8f90f7dcce00022dad5c
Denis Ziulikov <amznden@gmail.com>
2022-04-29 09:28:53 +0500

    Deleted reconnecting string

--- swapp/aws_mysql_db.py
+++ swapp/aws_mysql_db.py
@@ -79,7 +79,6 @@ def get_all_details_planet():
     """
     Getting data from the database
     """
-    conn.cursor()  # reconnecting mysql
     cur = conn.cursor()
     cur.execute("SELECT SQL_NO_CACHE * FROM planets")
     details_planet = cur.fetchall()
